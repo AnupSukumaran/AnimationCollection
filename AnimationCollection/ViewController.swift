4339506b1f8cc8df31b3349119429a2c36b60626
Anup Sukumaran <anup.sukumaran9@gmail.com>
2019-04-05 10:16:33 +0530

    Initial Commit

---
+++ AnimationCollection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimationCollection
+//
+//  Created by Qaptive Technologies on 05/04/19.
+//  Copyright © 2019 Qaptive Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
